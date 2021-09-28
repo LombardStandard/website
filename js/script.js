4a82c3385e3843c07fce68af922957b1f21cc29c
AMG22075 <52190087+AMG22075@users.noreply.github.com>
2021-09-28 12:09:19 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+var d = new Date();
+    var n = d.getFullYear();
+    document.getElementById("copyright").innerHTML = n;
+
 document.getElementById("openMenu").addEventListener("click", function () {
   var mobileMenu = document.getElementById("mobileMenu").style.opacity;
   if (mobileMenu == 0) {
@@ -8,7 +12,11 @@ document.getElementById("openMenu").addEventListener("click", function () {
     document.getElementById("mobileMenu").style.transform = "scale(0.9,0.9)";
     document.getElementById("mobileMenu").style.opacity = 0;
     document.getElementById("mobileMenu").style.pointerEvents = "none";
+    document.getElementById("requestMobile").style.display = "none";
+    document.getElementById("requestButton").style.opacity = 1;
+    document.getElementById("requestButton").style.pointerEvents = "auto";
   }
+  document.getElementById("requestMobile").style.display = "none";
 });
 
 window.onresize = closeMenuResize;
@@ -17,4 +25,7 @@ function closeMenuResize() {
   document.getElementById("mobileMenu").style.transform = "scale(0.9,0.9)";
   document.getElementById("mobileMenu").style.opacity = 0;
   document.getElementById("mobileMenu").style.pointerEvents = "none";
+  document.getElementById("requestMobile").style.display = "none";
+  document.getElementById("requestButton").style.opacity = 1;
+  document.getElementById("requestButton").style.pointerEvents = "auto";
 }
