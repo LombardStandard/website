6f654e0940d6ab306530cce839eee1a652bdb23a
AMG22075 <52190087+AMG22075@users.noreply.github.com>
2021-09-28 12:19:04 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var d = new Date();
-    var n = d.getFullYear();
-    document.getElementById("copyright").innerHTML = n;
+var n = d.getFullYear();
+document.getElementById("copyright").innerHTML = n;
 
 document.getElementById("openMenu").addEventListener("click", function () {
   var mobileMenu = document.getElementById("mobileMenu").style.opacity;
@@ -29,3 +29,15 @@ function closeMenuResize() {
   document.getElementById("requestButton").style.opacity = 1;
   document.getElementById("requestButton").style.pointerEvents = "auto";
 }
+
+_linkedin_partner_id = "3365265";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+(function () {
+  var s = document.getElementsByTagName("script")[0];
+  var b = document.createElement("script");
+  b.type = "text/javascript";
+  b.async = true;
+  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+  s.parentNode.insertBefore(b, s);
+})();
