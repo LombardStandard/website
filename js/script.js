d94f1e48806402d10268753a0dd34dd4f6164e2c
AMG22075 <52190087+AMG22075@users.noreply.github.com>
2021-10-04 12:11:20 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,32 +34,6 @@ document.addEventListener("click", function(e) {
     });
 });
 
-document.addEventListener("click", function(e) {
-    var container2 = document.getElementById("mobileMenu");
-    if (!container2.contains(e.target)) {
-        container2.style.transform = "scale(0.9,0.9)";
-        container2.style.opacity = 0;
-        container2.style.pointerEvents = "none";
-    }
-
-    document.getElementById("openMenu").addEventListener("click", function() {
-        var mobileMenu = document.getElementById("mobileMenu").style.opacity;
-        if (mobileMenu == 0) {
-            document.getElementById("mobileMenu").style.transform = "scale(1,1)";
-            document.getElementById("mobileMenu").style.opacity = 1;
-            document.getElementById("mobileMenu").style.pointerEvents = "auto";
-        } else {
-            document.getElementById("mobileMenu").style.transform = "scale(0.9,0.9)";
-            document.getElementById("mobileMenu").style.opacity = 0;
-            document.getElementById("mobileMenu").style.pointerEvents = "none";
-            document.getElementById("requestMobile").style.display = "none";
-            document.getElementById("requestButton").style.background = "#D1D5DB";
-            document.getElementById("requestButton").style.color = "#111827";
-            document.getElementById("requestButton").style.pointerEvents = "auto";
-            document.getElementById("requestButton").innerHTML = "REQUEST AN ACCOUNT";
-        }
-    });
-});
 
 document.getElementById("requestButton").addEventListener("click", function() {
     document.getElementById("requestMobile").style.display = "block";
